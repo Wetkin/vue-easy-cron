b51fa66f86d395bd70396c50b852d130bac9709f
Wetkin <m15800162183_1@163.com>
2019-07-09 19:37:30 +0800

    修改版本

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-   filename: 'vue-easy-cron.js' ,
+    filename: 'vue-easy-cron.js' ,
     library: 'vue-easy-cron', 
     libraryTarget: 'umd',
 	umdNamedDefine: true 
